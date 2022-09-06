a18567ab46db9f5353cfcb6c726596d4f65caeb6
Technisha <proxneon960@gmail.com>
2022-09-06 11:20:18 +0100

    It currently only generates an exit statement BUT HELL YEAH

--- src/backends/javatarget.nim
+++ src/backends/javatarget.nim
@@ -20,13 +20,19 @@ import
     pathutils
   ],
   std/[
-    os, tables
+    os, tables, strutils
   ]
 
 
 
 proc toJava*(graph:ModuleGraph, mlist:ModuleList) =
   var data = Data()
+  var javafile = newJavaFile(namespace="com.foc.nim.codegen")
+  var javaclass = newJavaClass("Main", true)
+  javafile.addJavaClass(javaclass)
+
+  var mainMethod = newJavaMethodDeclaration("main", "void", true, true)
+  javaclass.addClassMethod(mainMethod)
 
   for m in mlist.modules.items:
     if m == nil:
@@ -39,3 +45,22 @@ proc toJava*(graph:ModuleGraph, mlist:ModuleList) =
         continue
 
       n.scan(data)
+
+      var jc = ""
+
+      if nimfile.string.endsWith("quit.nim"):
+        if n.kind == TNodeKind.nkCall:
+          echo "Call statement found!"
+          for son in n.sons.items:
+            case son.kind
+            of nkSym:
+              if son.sym.name.s == "quit":
+                jc &= "System.exit"
+            of nkIntLit:
+              jc &= "(" & $son.intVal & ");"
+            else:
+              echo "Kind: ", n.kind
+
+        mainMethod.addSnippetToMethodBody jc.javacode
+
+  echo $javafile
