52e73555d65a54017ddd4a3df6de6124a3b7ac13
Technisha <proxneon960@gmail.com>
2022-09-06 13:29:25 +0100

    Current code, experimenting with symbol collection bound to a file

--- src/backends/javatarget.nim
+++ src/backends/javatarget.nim
@@ -24,23 +24,45 @@ import
   ]
 
 
+proc nodehandler(collectedSyms:var Table[string, seq[PSym]], file:AbsoluteFile, n:PNode) =
+  case n.kind
+  of nkCharLit:
+    discard
+
+  of nkIntLit..nkUInt64Lit:
+    discard
+
+  of nkFloatLit..nkFloat128Lit:
+    discard
+
+  of nkStrLit..nkTripleStrLit:
+    discard
+
+  of nkSym:
+    if n.sym notin collectedSyms[file.string]:
+      collectedSyms[file.string].add(n.sym)
+
+  of nkIdent:
+    discard
+
+  of nkEmpty:
+    discard
+
+  else:
+    for son in n.sons:
+      nodehandler(collectedSyms, file, son)
+
 
 proc toJava*(graph:ModuleGraph, mlist:ModuleList) =
   var data = Data()
-  var javafile = newJavaFile(namespace="com.foc.nim.codegen")
-  var javaclass = newJavaClass("Main", true)
-  javafile.addJavaClass(javaclass)
-
-  var mainMethod = newJavaMethodDeclaration("main", "void", true, true)
-  javaclass.addClassMethod(mainMethod)
-
-  mainMethod.addMethodArgument("String[]", "args")
+  var collectedSyms:Table[string, seq[PSym]]
 
   for m in mlist.modules.items:
     if m == nil:
       continue
 
     let nimfile = AbsoluteFile toFullPath(graph.config, m.sym.position.FileIndex)
+    collectedSyms[nimfile.string] = newSeq[PSym]()
 
     for n in m.nodes.items:
       if n == nil:
@@ -48,21 +70,12 @@ proc toJava*(graph:ModuleGraph, mlist:ModuleList) =
 
       n.scan(data)
 
-      var jc = ""
+      nodehandler(collectedSyms, nimfile, n)
 
-      if nimfile.string.endsWith("quit.nim"):
-        if n.kind == TNodeKind.nkCall:
-          echo "Call statement found!"
-          for son in n.sons.items:
-            case son.kind
-            of nkSym:
-              if son.sym.name.s == "quit":
-                jc &= "System.exit"
-            of nkIntLit:
-              jc &= "(" & $son.intVal & ");"
-            else:
-              echo "Kind: ", n.kind
+  var total = 0
+  for key in collectedSyms.keys:
+    echo "Collected symbols from `", key, "`: ", collectedSyms[key].len
+    total += collectedSyms[key].len
 
-        mainMethod.addSnippetToMethodBody jc.javacode
-
-  echo $javafile
+  echo "My collected symbols: ", total
+  echo "Other collected symbols: ", data.syms.data.len
